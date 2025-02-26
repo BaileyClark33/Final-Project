38d58bed828ad051c2e2883b965d18c500f4b195
BaileyClark33 <bwc339@gmail.com>
2025-02-26 22:34:56 +0000

    g

--- modules/light_sensor/light_sensor.cpp
+++ modules/light_sensor/light_sensor.cpp
@@ -11,6 +11,9 @@
 
 //=====[Declaration of private defines]========================================
 
+#define SAMPLESIZE 10
+#define DOORCLOSEDLEVEL 0.5
+
 //=====[Declaration of private data types]=====================================
 
 //=====[Declaration and initialization of public global objects]===============
@@ -23,23 +26,43 @@ DigitalIn lightSensor( PA_3 );
 
 //=====[Declaration and initialization of private global variables]============
 
-static bool sensorState = OFF;
+static bool sensorState;
+float lightValues[SAMPLESIZE];
 
 //=====[Declarations (prototypes) of private functions]========================
 
 //=====[Implementations of public functions]===================================
 
 void sensorInit() {
-    
+    sensorState = OFF;
 }
 
 bool sensorRead() {
     return sensorState;
 }
 
-
 void sensorUpdate() {
-    
+    float temp = lightSensor.read();
+    for (int i = 0; i < SAMPLESIZE; i++) {
+        if (i == 0) {
+            lightSensor[i] = temp;
+        } else {
+            lightSensor[i] = lightSensor[i-1];
+        }
+    }
+
+    int total = 0;
+    for (int i = 0; i < SAMPLESIZE; i++) {
+        total += lightSensor[i];
+    }
+
+    int average = total/SAMPLESIZE;
+    if (average < DOORCLOSEDLEVEL) {
+        sensorState = ON;
+    } else {
+        sensorState = OFF;
+    }
 }
+
 
 //=====[Implementations of private functions]==================================
